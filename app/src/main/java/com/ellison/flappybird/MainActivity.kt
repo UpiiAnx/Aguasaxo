17b7923cece33dc9b761f715eeaf459c43d9e04d
Angr054 <148915049+Angr054@users.noreply.github.com>
2025-04-09 23:19:51 -0600

    Initial commit

---
+++ app/src/main/java/com/ellison/flappybird/MainActivity.kt
@@ -0,0 +1,116 @@
+package com.ellison.flappybird
+
+import Clickable
+import GameScreen
+import GameViewModel
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.graphics.Path
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.animation.AnticipateInterpolator
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.annotation.RequiresApi
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.ellison.flappybird.model.GameAction
+import com.ellison.flappybird.model.GameStatus
+import com.ellison.flappybird.ui.theme.FlappyBirdTheme
+import com.ellison.flappybird.util.StatusBarUtil
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
+
+class MainActivity : ComponentActivity() {
+    private val viewModel: GameViewModel by viewModels()
+
+    // `@RequiresApi(Build.VERSION_CODES.S)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Expand screen to status bar.
+        StatusBarUtil.transparentStatusBar(this)
+
+        setContent {
+            FlappyBirdTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    val gameViewModel: GameViewModel = viewModel()
+
+                    // Send a auto tick action to view model and trigger game start.
+                    LaunchedEffect(key1 = Unit) {
+                        while (isActive) {
+                            delay(AutoTickDuration)
+                            if (gameViewModel.viewState.value.gameStatus != GameStatus.Waiting) {
+                                gameViewModel.dispatch(GameAction.AutoTick)
+                            }
+                        }
+                    }
+
+                    val lifecycleOwner = LocalLifecycleOwner.current
+                    DisposableEffect(key1 = Unit) {
+                        val observer = object : LifecycleObserver {
+                            @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+                            fun onPause() {
+                                // Todo send pause action
+                            }
+
+                            @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+                            fun onResume() {
+                                // Todo send resume action
+                            }
+                        }
+
+                        lifecycleOwner.lifecycle.addObserver(observer)
+                        onDispose {
+                            lifecycleOwner.lifecycle.removeObserver(observer)
+                        }
+                    }
+
+                    Flappy(Clickable(
+
+                        onStart = {
+                            gameViewModel.dispatch(GameAction.Start)
+                        },
+
+                        onTap = {
+                            gameViewModel.dispatch(GameAction.TouchLift)
+                        },
+
+                        onRestart = {
+                            gameViewModel.dispatch(GameAction.Restart)
+                        },
+
+                        onExit = {
+                            finish()
+                        }
+                    ))
+                }
+            }
+        }
+
+
+        // Log.d("Splash", "onCreate() splashScreen:${getSplashScreen()}}")
+    }
+}
+
+@Composable
+fun Flappy(clickable: Clickable = Clickable()) {
+    GameScreen(clickable = clickable)
+}
+
+const val AutoTickDuration = 50L // 300L Control bird and pipe speed.
